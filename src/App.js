f07f6d78c8eef2e5754349051ba78fe7ea6f0ed4
isabelpm <isabelpuig90@gmail.com>
2021-03-05 10:48:51 +0100

    firts commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// Importamos los archivos creados
+import Book from './components/Book'
+
+function App() {
+
+    return (
+        <Book />
+    )
+}
+export default App
---
+++ src/components/book.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Book () {
+    return (
+        <div>
+            <h2>Viatge a la lluna</h2>
+        </div>
+    )
+}
+export default Book
